b4dc31fbef893086147957214a44eed15ff3151f
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2022-06-02 08:46:30 -0700

    Trying something

--- TextBattlePart2b/src/Player.java
+++ TextBattlePart2b/src/Player.java
@@ -120,7 +120,7 @@ public class Player
 	//attacks the monster
 	public int attack(Monster monster)
 	{
-		int attackDmg = (int)((Math.random()*(maxDmg-minDmg+1) + minDmg));
+		int attackDmg = (int)((Math.random()*(maxDmg-minDmg) + minDmg));
 		monster.takeDamage(attackDmg);
 		return attackDmg;
 	}
